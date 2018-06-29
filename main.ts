dd31672168c728e20493d06366cabcf0c7d5a207
Dennis Janiak <dennis.janiak@googlemail.com>
2018-06-29 11:15:24 +0200

    Configuration for declaration typescript files and npm packaging.

---
+++ main.ts
@@ -0,0 +1,5 @@
+// Barrel file to reexport the relevant declarations for the consumer.
+// This allows simpler imports without specifying the path inside the project (implementation detail).
+
+export * from './src/EqualityComparer';
+export * from './src/Memoizer';
